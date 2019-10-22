9b8ef676bb563fbd5910c528ee4ff20db7e719e9
igorromao <igoroamo@hotmail.com>
2019-10-22 14:55:00 -0300

    Projeto 0.1 Loja

---
+++ LojaVirtual/LojaVirtual/Controllers/ProdutoController.cs
@@ -0,0 +1,34 @@
+﻿using LojaVirtual.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Controllers
+{
+    public class ProdutoController : Controller
+    {
+
+        public ActionResult Visualizar()
+
+        {
+
+            Produto produto = GetProduto();
+            return View(produto);
+
+            //return new ContentResult { } { Content =}
+        }
+
+        private Produto GetProduto()
+        {
+            return new Produto()
+            {
+                Id = 1,
+                Nome = "play 4",
+                Descricao = "jogo 5k",
+                Valor = 20.00M
+            };
+        }
+    }
+}
