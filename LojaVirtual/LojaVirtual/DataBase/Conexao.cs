3feb3d750e3ba3cbd8d0e92b3893392ef902a9ee
igorromao <igoroamo@hotmail.com>
2019-10-24 15:29:42 -0300

    Projeto-Final-Ate_banco
    Final

---
+++ LojaVirtual/LojaVirtual/DataBase/Conexao.cs
@@ -0,0 +1,73 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+
+namespace LojaVirtual.DataBase
+{
+    public class Conexao
+    {
+        private const string ConnectionString = "Server=localhost; Database=usuarios; Uid=igor; Pwd=852310;";
+        private MySqlConnection myConn;
+
+        public Conexao()
+        {
+            myConn = new MySqlConnection(ConnectionString);
+        }
+
+        public void Fechar()
+        {
+            if (myConn.State != System.Data.ConnectionState.Closed)
+                myConn.Close();
+        }
+
+        public void Executar(MySqlCommand cmd)
+        {
+            try
+            {
+                if (myConn.State != System.Data.ConnectionState.Open)
+                    myConn.Open();
+
+                cmd.Connection = myConn;
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException e)
+            {
+                switch (e.Number)
+                {
+                    case 1062: throw new Exception("Este registro já está sendo utilizado.");
+                    case 1451: throw new Exception("Atenção! Este registro não pode ser excluído no momento (chave estrangeira).");
+                    default: throw;
+                }
+            }
+            finally
+            {
+                Fechar();
+            }
+        }
+
+
+
+        public DataTable Pesquisar(MySqlCommand cmd)
+        {
+            try
+            {
+                cmd.Connection = myConn;
+
+                DataTable dt = new DataTable();
+
+                MySqlDataAdapter adpt = new MySqlDataAdapter(cmd);
+
+                adpt.Fill(dt);
+
+                return dt;
+            }
+            catch (Exception)
+            {
+                Fechar();
+                throw;
+            }
+
+        }
+
+    }
+}
---
+++ LojaVirtual/LojaVirtual/DataBase/LojaVirtualContext.cs
@@ -0,0 +1,21 @@
+﻿using LojaVirtual.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.DataBase
+{
+    public class LojaVirtualContext : DbContext
+    {
+
+        public LojaVirtualContext(DbContextOptions<LojaVirtualContext> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<Cliente> Clientes { get; set; }  //criando o Dbset, para falar pro entity criar a tabela cliente no banco de dados
+    }
+}
